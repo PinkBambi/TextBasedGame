0d073f020b5c387b377b2746db6ce067a4db6981
Maggie <margaritakarlsson@yahoo.se>
2016-05-03 14:32:13 +0200

    Changed CSS to green and fixed function on enter

--- js/main.js
+++ js/main.js
@@ -8,7 +8,13 @@ app.controller('playerAnwers', function($scope) {
         'What do you do first?',
         'Search for: people, food or shelter?'
     ];
-
+    /************************/
+    $("#thePlace").keyup(function(e) {
+        if (e.keyCode == 13) {
+            $scope.add();
+        }
+    });
+    /************************/
     $scope.add = function() {
         console.log("Spelare skrev: " + $scope.input);
         var lowerResult = $scope.input.toLowerCase();
@@ -17,7 +23,7 @@ app.controller('playerAnwers', function($scope) {
                 'Your gut feeling told you to try your luck and search for people. While you are stumbling in the dark woods a unknown presence is felt behind you.',
                 'What is your reaction when a hand is placed on your shoulder?',
                 'Do you: Scream, fight or run?');
-                 ActionHand();
+            ActionHand();
         } else if (lowerResult === "food") {
             $scope.items.push(
                 'balalala',
@@ -32,29 +38,33 @@ app.controller('playerAnwers', function($scope) {
         $scope.input = '';
     };
 
-     function ActionHand() {
-      var lowerResult2 = $scope.input.toLowerCase();
-      if (lowerResult2 === "scream") {
-          $scope.items.push(
-              'GE daj',
-              'GE daj?',
-              'Do you: GE daj, GE daj or GE daj?');
-              ActionHand();
-      } else if (lowerResult2 === "fight") {
-          $scope.items.push(
-              'Gå på en promenad',
-              'Gå på en promenad?',
-              'Do you: Gå på en promenad, Gå på en promenad or Gå på en promenad?');
-      } else if (lowerResult2 === "run") {
-        console.log('brsss');
-          $scope.items.push(
-              'falla fel',
-              'inte falla rätt?',
-              'Do you: ha, ha or ha?');
-      }
+    /**********/
+    function ActionHand() {
+        var lowerResult2 = $scope.input.toLowerCase();
+        if (lowerResult2 === "scream") {
+            $scope.items.push(
+                'GE daj',
+                'GE daj?',
+                'Do you: GE daj, GE daj or GE daj?');
+            ActionHand();
+        } else if (lowerResult2 === "fight") {
+            $scope.items.push(
+                'Gå på en promenad',
+                'Gå på en promenad?',
+                'Do you: Gå på en promenad, Gå på en promenad or Gå på en promenad?');
+        } else if (lowerResult2 === "run") {
+            console.log('brsss');
+            $scope.items.push(
+                'falla fel',
+                'inte falla rätt?',
+                'Do you: ha, ha or ha?');
+        }
     }
 
+
+    /****************/
     /*$scope.remove = function(index) {
         $scope.items.splice(index, 1);
       };*/
 });
+/**********/
